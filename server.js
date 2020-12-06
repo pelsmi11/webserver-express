c0609dde4c8c65586424b37a5043634357f7b211
pelsmi11 <hmartinezmoreira81@gmail.com>
2020-12-06 11:14:56 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', function(req, res) {
+
+    res.render('home', {
+        nombre: 'Hector',
+        anio: new Date().getFullYear()
+    });
+})
+app.get('/about', (req, res) => {
+
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
+})
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+});
